a4b14fca8154538bb6000054b0385296a5dc85eb
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-18 13:46:11 -0600

    Feat: uso de alias

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SideMenuComponent } from "../shared/side-menu/side-menu.component";
+import { SideMenuComponent } from '@shared/side-menu/side-menu.component';
 
 @Component({
   imports: [RouterModule, SideMenuComponent],
