992cbb75ccc0f819ad38761e0afcbcfab389745e
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-18 12:43:20 -0600

    Feat: estructura de directorios base

---
+++ src/app/shared/heavy-loadres/heavy-loadres-slow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loader-slow',
+  imports: [],
+  template: `
+    <h1>Hola mundo</h1>
+  `
+})
+export class HeavyLoaderSlowComponent {
+
+}
---
+++ src/app/shared/heavy-loadres/user-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  imports: [],
+  template: `
+    <h1>Hola mundo</h1>
+  `
+})
+export class UserLoaderComponent {
+
+}
