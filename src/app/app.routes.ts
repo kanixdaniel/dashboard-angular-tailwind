176e7625edc8ea658395f85ed8e4ab2949d41889
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-19 15:53:39 -0600

    Feat: instalación de Angular Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -51,6 +51,11 @@ export const routes: Routes = [
                 loadComponent: () => import('./dashboard/pages/input-output/input-output.component')
             },
             {
+                path: 'material-components',
+                title: 'Material Components',
+                loadComponent: () => import('./dashboard/pages/material/material.component')
+            },
+            {
                 path: '',
                 redirectTo: 'control-flow',
                 pathMatch: 'full'
---
+++ src/app/dashboard/pages/material/material.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from "../../../shared/title/title.component";
+
+@Component({
+  selector: 'app-material',
+  imports: [TitleComponent],
+  templateUrl: './material.component.html',
+  styles: ``
+})
+export default class MaterialComponent {
+
+}
