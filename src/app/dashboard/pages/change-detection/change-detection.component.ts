67ae07988471bd55649f3cbba778d057adeaf846
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-18 18:52:34 -0600

    Feat: change detection con signals VS ciclos de vida de Angular (ZoneJs)

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from "../../../shared/title/title.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule, TitleComponent],
+    styles: ``,
+    template: `
+    <app-title [title]="currectFramework()" />
+    <pre>{{frameworkAsSignal() | json}}</pre>
+    <pre>{{frameworkAsProperty | json}}</pre>
+  `
 })
 export default class ChangeDetectionComponent {
+    currectFramework = computed(
+        () => `Change Detection - ${this.frameworkAsSignal().name}`
+    )
+    frameworkAsSignal = signal({
+        name: 'Angular',
+        releaseDate: 2016
+    });
+    frameworkAsProperty = {
+        name: 'Angular',
+        releaseDate: 2016
+    };
 
+    constructor() {
+        setTimeout(() => {
+            // this.frameworkAsProperty.name = 'React';
+            this.frameworkAsSignal.update(value => {
+                value.name = 'Astro';
+
+                return {...value};
+            });
+            console.log('Hecho');
+        }, 3000);
+    }
 }
