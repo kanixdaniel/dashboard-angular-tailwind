09ee9bc19fa300aa8de879318c351c13aa752449
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-19 17:13:53 -0600

    Feat: uso de componentes de Agular Material 3

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { TitleComponent } from "../../../shared/title/title.component";
+import { Component, inject } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { OptionsBottomSheetComponent } from './ui/options-bottom-sheet/options-bottom-sheet.component';
 
 @Component({
-  selector: 'app-material',
-  imports: [TitleComponent],
-  templateUrl: './material.component.html',
-  styles: ``
+    selector: 'app-material',
+    imports: [
+        TitleComponent,
+        MatSlideToggleModule,
+        MatIconModule,
+        MatButtonModule,
+        MatBadgeModule,
+        MatBottomSheetModule
+    ],
+    templateUrl: './material.component.html',
+    styles: ``
 })
 export default class MaterialComponent {
+    private _bottomSheet = inject(MatBottomSheet);
 
+    openBottomSheet(): void {
+        this._bottomSheet.open(OptionsBottomSheetComponent);
+    }
 }
---
+++ src/app/dashboard/pages/material/ui/options-bottom-sheet/options-bottom-sheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+    selector: 'app-options-bottom-sheet',
+    imports: [MatListModule],
+    templateUrl: './options-bottom-sheet.component.html',
+    styles: ``
+})
+export class OptionsBottomSheetComponent {
+
+}
