0209dafbb484051e3bdbf3fd6f6539d12c4325c6
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-19 15:16:29 -0600

    Feat: generacion automatica de productos

--- src/app/dashboard/pages/input-output/input-output.component.ts
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, signal } from '@angular/core';
 import { ProductCardComponent } from './ui/product-card/product-card.component';
+import { IProduct } from '@interfaces/product.interface';
+import { interval, take, tap } from 'rxjs';
 
 @Component({
     selector: 'app-input-output',
@@ -7,6 +9,38 @@ import { ProductCardComponent } from './ui/product-card/product-card.component';
     templateUrl: './input-output.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class InputOutputComponent {
+export default class InputOutputComponent implements OnDestroy {
+    products = signal<IProduct[]>([
+        {
+            id: 1,
+            name: 'Producto 1',
+            quantity: 0
+        },
+        {
+            id: 2,
+            name: 'Producto 2',
+            quantity: 0
+        },
+    ]);
 
+    private intervalSubscription = interval(1000)
+        .pipe(
+            tap(() => {
+                this.products.update((products => [
+                    ...products,
+                    {
+                        id: products.length + 1,
+                        name: `Product ${products.length + 1}`,
+                        quantity: 0
+                    }
+                ]))
+            }),
+            take(5)
+        )
+        .subscribe();
+
+
+    ngOnDestroy(): void {
+        this.intervalSubscription.unsubscribe();
+    }
 }
